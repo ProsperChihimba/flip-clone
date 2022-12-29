e75a543f1ca8328e0ba82969417c92b90a8d754d
ProsperChihimba <prosperchihimba@gmail.com>
2022-12-30 01:56:56 +0400

    Add: second row

--- lib/pages/parts/balance.dart
+++ lib/pages/parts/balance.dart
@@ -12,66 +12,91 @@ class BalanceSection extends StatelessWidget {
         horizontal: AppLayout.getWidth(17),
       ),
       child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(
-              AppLayout.getHeight(25),
-            ),
-            color: Colors.black,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            AppLayout.getHeight(25),
           ),
-          padding: EdgeInsets.symmetric(
-            horizontal: AppLayout.getWidth(20),
-            vertical: AppLayout.getHeight(20),
-          ),
-          height: AppLayout.getHeight(250),
-          child: Column(
-            children: [
-              // First row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "SPEND WALLET",
-                    style: TextStyle(
-                      fontSize: AppLayout.getHeight(10),
-                      letterSpacing: 3,
-                      fontFamily: 'Monument Extended',
-                      color: Colors.white,
+          color: Colors.black,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: AppLayout.getWidth(20),
+          vertical: AppLayout.getHeight(20),
+        ),
+        height: AppLayout.getHeight(250),
+        child: Column(
+          children: [
+            // First row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "SPEND WALLET",
+                  style: TextStyle(
+                    fontSize: AppLayout.getHeight(10),
+                    letterSpacing: 3,
+                    fontFamily: 'Monument Extended',
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: AppLayout.getHeight(12),
+                    horizontal: AppLayout.getWidth(20),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      AppLayout.getHeight(20),
+                    ),
+                    gradient: const LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color.fromARGB(58, 127, 0, 246),
+                        Color.fromARGB(255, 177, 4, 245),
+                      ],
                     ),
                   ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      vertical: AppLayout.getHeight(12),
-                      horizontal: AppLayout.getWidth(20),
+                  child: Text(
+                    "Save Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: AppLayout.getHeight(13),
+                      fontWeight: FontWeight.w500,
                     ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        AppLayout.getHeight(20),
-                      ),
-                      gradient: const LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Color.fromARGB(58, 127, 0, 246),
-                          Color.fromARGB(255, 177, 4, 245),
-                        ],
-                      ),
-                    ),
-                    child: Text(
-                      "Save Now",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: AppLayout.getHeight(13),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
+            ),
 
-              // Second row
-              Gap(AppLayout.getHeight(20)),
-            ],
-          )),
+            // Second row
+            Gap(AppLayout.getHeight(20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: AppLayout.getHeight(20)),
+                  child: Text(
+                    "USDT",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: AppLayout.getHeight(11),
+                    ),
+                  ),
+                ),
+                Text(
+                  "0",
+                  style: TextStyle(
+                    fontFamily: "Monument Extended",
+                    fontSize: AppLayout.getHeight(40),
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
