4957601344dd2f462daef397eda4b34909c11959
ProsperChihimba <prosperchihimba@gmail.com>
2022-12-30 01:42:53 +0400

    Add: save card first row

--- lib/pages/parts/balance.dart
+++ lib/pages/parts/balance.dart
@@ -1,5 +1,6 @@
 import 'package:flip/utils/app_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BalanceSection extends StatelessWidget {
   const BalanceSection({super.key});
@@ -11,9 +12,66 @@ class BalanceSection extends StatelessWidget {
         horizontal: AppLayout.getWidth(17),
       ),
       child: Container(
-        height: AppLayout.getHeight(200),
-        color: Colors.black,
-      ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(
+              AppLayout.getHeight(25),
+            ),
+            color: Colors.black,
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: AppLayout.getWidth(20),
+            vertical: AppLayout.getHeight(20),
+          ),
+          height: AppLayout.getHeight(250),
+          child: Column(
+            children: [
+              // First row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "SPEND WALLET",
+                    style: TextStyle(
+                      fontSize: AppLayout.getHeight(10),
+                      letterSpacing: 3,
+                      fontFamily: 'Monument Extended',
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: AppLayout.getHeight(12),
+                      horizontal: AppLayout.getWidth(20),
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        AppLayout.getHeight(20),
+                      ),
+                      gradient: const LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color.fromARGB(58, 127, 0, 246),
+                          Color.fromARGB(255, 177, 4, 245),
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      "Save Now",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: AppLayout.getHeight(13),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+
+              // Second row
+              Gap(AppLayout.getHeight(20)),
+            ],
+          )),
     );
   }
 }
