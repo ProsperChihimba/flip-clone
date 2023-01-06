814ac4d688279884340b742abb46644f69cc903f
ProsperChihimba <prosperchihimba@gmail.com>
2023-01-06 13:31:37 +0400

    Update: sendmoney bg color

--- lib/pages/send/index.dart
+++ lib/pages/send/index.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/app_layout.dart';
+
 class SendMoney extends StatefulWidget {
   const SendMoney({super.key});
 
@@ -10,6 +12,13 @@ class SendMoney extends StatefulWidget {
 class _SendMoneyState extends State<SendMoney> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 148, 253, 166),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: AppLayout.getWidth(17),
+        ),
+      ),
+    );
   }
 }
