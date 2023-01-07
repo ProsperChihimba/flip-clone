d326710cd300b32ab07e6b174338c0bab565eb68
ProsperChihimba <prosperchihimba@gmail.com>
2023-01-07 19:24:04 +0400

    Add: proceed button in airtime

--- lib/pages/payments/airtime/index.dart
+++ lib/pages/payments/airtime/index.dart
@@ -54,6 +54,36 @@ class _AirtimeScreenState extends State<AirtimeScreen> {
           ],
         ),
       ),
+
+      // button
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(
+          bottom: AppLayout.getHeight(20),
+          right: AppLayout.getWidth(18),
+          left: AppLayout.getWidth(18),
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(23)),
+          //
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [
+                Color.fromARGB(255, 81, 235, 107),
+                Color.fromARGB(255, 207, 255, 160),
+              ],
+            ),
+            borderRadius: BorderRadius.circular(30),
+          ),
+
+          child: const Text(
+            "Proceed",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
