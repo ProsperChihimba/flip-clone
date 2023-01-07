291df1063684ab9c843a3b593de3444a04a591b0
ProsperChihimba <prosperchihimba@gmail.com>
2023-01-07 19:18:36 +0400

    Finish up chose provider intial design

--- lib/pages/payments/airtime/chooseProvider.dart
+++ lib/pages/payments/airtime/chooseProvider.dart
@@ -1,3 +1,5 @@
+import 'package:flip/utils/app_layout.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
 class ChooseProvider extends StatefulWidget {
@@ -10,6 +12,40 @@ class ChooseProvider extends StatefulWidget {
 class _ChooseProviderState extends State<ChooseProvider> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: AppLayout.getHeight(20),
+        horizontal: AppLayout.getWidth(20),
+      ),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(AppLayout.getHeight(40)),
+        border: Border.all(
+          width: AppLayout.getWidth(5),
+          color: Colors.grey.shade300,
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // text
+          Text(
+            "Choose Provider",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: AppLayout.getHeight(17),
+            ),
+          ),
+
+          // icon
+          Icon(
+            FluentSystemIcons.ic_fluent_chevron_down_circle_filled,
+            size: AppLayout.getHeight(30),
+            color: const Color(0xFFa8ff79),
+          )
+        ],
+      ),
+    );
   }
 }
