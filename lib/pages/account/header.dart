2961fe14e45eb1dc48453bb51796d5c3c1d51392
ProsperChihimba <prosperchihimba@gmail.com>
2023-01-09 23:45:20 +0400

    Add: account header section

---
+++ lib/pages/account/header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountHeader extends StatefulWidget {
+  const AccountHeader({super.key});
+
+  @override
+  State<AccountHeader> createState() => _AccountHeaderState();
+}
+
+class _AccountHeaderState extends State<AccountHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/account/index.dart
+++ lib/pages/account/index.dart
@@ -1,4 +1,7 @@
+import 'package:flip/pages/account/header.dart';
+import 'package:flip/utils/app_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -10,6 +13,21 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(17)),
+
+        //
+        child: ListView(
+          children: [
+            Gap(AppLayout.getHeight(30)),
+
+            // header
+            const AccountHeader(),
+          ],
+        ),
+      ),
+    );
   }
 }
