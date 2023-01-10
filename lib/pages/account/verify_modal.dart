0c84888347ec6b9ecc5b9cab22376318baef6313
ProsperChihimba <prosperchihimba@gmail.com>
2023-01-10 12:25:51 +0400

    Update: verify account modal initial design

--- lib/pages/account/verify_modal.dart
+++ lib/pages/account/verify_modal.dart
@@ -1,4 +1,6 @@
+import 'package:flip/utils/app_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class VerifyAccountModal extends StatelessWidget {
   const VerifyAccountModal({super.key});
@@ -6,7 +8,32 @@ class VerifyAccountModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("data"),
+      padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(25)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // center small container
+          Gap(AppLayout.getHeight(5)),
+          Container(
+            height: AppLayout.getHeight(7),
+            width: AppLayout.getWidth(70),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+
+          // image
+          Gap(AppLayout.getHeight(22)),
+          Image(
+            image: const AssetImage(
+              'assets/card.png',
+            ),
+            width: AppLayout.getWidth(180),
+          ),
+        ],
+      ),
     );
   }
 }
